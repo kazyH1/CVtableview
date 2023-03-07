99efa9f02f511c34654c2989b109cbac54892706
maytinhbotui <maytinhbotui>
2023-03-08 00:12:40 +0700

    init project

--- CVtableview/CVtableview/ViewController.swift
+++ CVtableview/CVtableview/ViewController.swift
@@ -7,19 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
-    struct Data {
-        var nameTitle: String
-        var nameLbName: String
-        var img : String
-        var Detail: String
-        var SDT: Int
-        
-        
+struct Data {
+    var nameTitle: String
+    var nameLbName: String
+    var img : String
+    var Detail: String
+    var SDT: String
+    
+    
+}
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource, TableViewCellDelegate {
+    
+    func makeCall(with phoneNumber: String) {
+        guard let url = URL(string: "tel://\(phoneNumber)") else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-    var data = [Data(nameTitle: "Nguyen Duc Huy",nameLbName: "Student at DTU",img: "Image1", Detail: "Date of birth: 2002/10/16\nAddress: Quang Nam\nZodiac: Libra",SDT: 0944521168),Data(nameTitle: "Nguyen Duc Minh",nameLbName: "Working at DNTN NguyenMinhHuy",img: "Image2", Detail: "Date of birth: 2000/12/25\nAddress: Quang Nam\nZodiac: Capricorn",SDT: 0948981678)]
-    @IBOutlet weak var tbvCV: UITableView!{
-        didSet{
+    
+    @IBOutlet weak var tbvCV: UITableView! {
+        didSet {
             tbvCV.delegate = self
             tbvCV.dataSource = self
             tbvCV.register(UINib(nibName: "CVTableViewCell", bundle: nil), forCellReuseIdentifier: "cell1")
@@ -28,53 +34,78 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         }
     }
     
+    var data = [Data(nameTitle: "Nguyen Duc Huy",nameLbName: "Student at DTU",img: "Image1", Detail: "Date of birth: 2002/10/16\nAddress: Quang Nam\nZodiac: Libra",SDT: "0944521168"),Data(nameTitle: "Nguyen Duc Minh",nameLbName: "Working at DNTN NguyenMinhHuy",img: "Image2", Detail: "Date of birth: 2000/12/25\nAddress: Quang Nam\nZodiac: Capricorn",SDT: "0948981678")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-                        // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            
+            //AlertController(ValueTitle: data[indexPath.section].nameTitle, ValueMessage: data[indexPath.section].Detail, NameTitleAction: "OK")
+            let alert = UIAlertController(title: data[indexPath.section].nameTitle, message: data[indexPath.section].Detail, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                switch action.style{
+                    case .default:
+                    print("default")
+
+                    case .cancel:
+                    print("cancel")
+
+                    case .destructive:
+                    print("destructive")
+
+                }
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-       
-        if indexPath.row == 0
-        {
-            let cell = tbvCV.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! CVTableViewCell
+        if indexPath.row == 0 {
+            let cell = registerCell(valueInput: "cell1", indexPath: indexPath) as! CVTableViewCell
+            //let cell = tbvCV.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! CVTableViewCell
             cell.lbTitle.text=data[indexPath.section].nameTitle
             cell.lbName.text=data[indexPath.section].nameLbName
             return cell
-        }else if indexPath.row == 1
-        {
-        let cell = tbvCV.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! imageTableViewCell
-        cell.imgUser.image=UIImage(named: data[indexPath.section].img)
-            
+        } else if indexPath.row == 1 {
+            let cell = registerCell(valueInput: "cell2", indexPath: indexPath) as! imageTableViewCell
+            cell.imgUser.image=UIImage(named: data[indexPath.section].img)
             cell.lbDetail.text = data[indexPath.section].Detail
             return cell
             
-        }
-        else  {
-            let cell = tbvCV.dequeueReusableCell(withIdentifier: "cell3", for: indexPath) as! buttonTableViewCell
+        } else {
+            let cell = registerCell(valueInput: "cell3", indexPath: indexPath) as! buttonTableViewCell
             cell.imgBackground.image=UIImage(named: "Image3")
-            /*cell.btnCall =  URL(string: "tel://" + data[indexPath.section].SDT ) else { return }
-            UIApplication.shared.open(data[indexPath.section].SDT )*/
+            let phoneNumber = data[indexPath.section].SDT
+            cell.phoneNumber = phoneNumber
+            cell.delegate = self
             return cell
-            }
+        }
     }
+        
+    func registerCell(valueInput: String, indexPath: IndexPath) -> UITableViewCell {
+        let cell = tbvCV.dequeueReusableCell(withIdentifier: valueInput, for: indexPath)
+        return cell
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0{
+        if indexPath.row == 0 {
             return 100
-        }
-        else if indexPath.row == 1{
+        } else if indexPath.row == 1 {
             return 250
-        }
-        else {
+        } else {
             return 170
-            
         }
     }
 }
